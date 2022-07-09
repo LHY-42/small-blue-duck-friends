2e7e8f9359ff7828d01c7dc725b82637382ac71c
faith <89432125+faithchongrui@users.noreply.github.com>
2022-07-09 11:26:28 +0800

    sdss

---
+++ small blue duck/small blue duck/NewFriendView.swift
@@ -0,0 +1,22 @@
+//
+//  NewFriendView.swift
+//  small blue duck
+//
+//  Created by FAITH CHONG RUI EN stu on 9/7/22.
+//
+
+import SwiftUI
+
+struct NewFriendView: View {
+    
+    @State var
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewFriendView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewFriendView()
+    }
+}
